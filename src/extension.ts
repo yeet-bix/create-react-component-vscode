78266d13f7ba9e1657656572b30753750cbc4d57
Liam Ketchup <kiam.letchup@gmail.com>
2019-07-23 15:48:32 +1000

    Generate 'Hello, world!' extension using yo code

---
+++ src/extension.ts
@@ -0,0 +1,11 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+	const disposable = vscode.commands.registerCommand('extension.createReactComponent', () => {
+		vscode.window.showInformationMessage('Hello World!');
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+export function deactivate() {}
