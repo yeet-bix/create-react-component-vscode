b6a4589a83e2dee02b98920c0b444b555aa8b753
Liam Ketchup <kiam.letchup@gmail.com>
2019-07-24 08:33:39 +1000

    Hide create component command in the command palette; show only when right clicking a folder.

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,23 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-	const disposable = vscode.commands.registerCommand('extension.createReactComponent', () => {
-		vscode.window.showInformationMessage('Hello World!');
-	});
+    const disposable = vscode.commands.registerCommand(
+        'extension.createReactComponent',
+        async (uri: vscode.Uri) => {
+            const componentName = await vscode.window.showInputBox({
+                prompt: `Create component in ${uri.path}`,
+                placeHolder: 'MyComponent',
+            });
 
-	context.subscriptions.push(disposable);
+            if (componentName !== undefined && componentName.length > 0) {
+                vscode.window.showInformationMessage(componentName);
+            } else {
+                vscode.window.showErrorMessage('Must provide component name');
+            }
+        },
+    );
+
+    context.subscriptions.push(disposable);
 }
 
 export function deactivate() {}
